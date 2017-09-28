d39282130e85732582acdbea03e6101609bbed08
Ramon Santos Malaquias <f043684@intrajus.tjrn>
2017-09-28 11:37:37 -0300

    Ajustes no LocalPortScanner e Implementação da primeira versão do RemotePortScanner

--- src/br/com/imd/protocolos/tcp/portscanner/local/LocalPortScanner.java
+++ src/br/com/imd/protocolos/tcp/portscanner/local/LocalPortScanner.java
@@ -7,19 +7,22 @@ public class LocalPortScanner {
 
 	public static int porta = 0;
 	public static int portaFinal = 65535;
+	public static String mensagemErroEnderecoEmUso = "Address already in use: JVM_Bind";
 
 	public static void main(String[] args) throws IOException {
-		System.out.println("--> Iniciando escaneamento de portas local.");
+		System.out.println("--> Iniciando escaneamento local de portas.");
 		while (porta <= portaFinal) {
 			try {
 				ServerSocket connectionSocket = new ServerSocket(porta);
 			} catch (Exception e) {
-				System.out.println("Porta " + porta + " está em uso!");
+				if (e.getMessage().equals(mensagemErroEnderecoEmUso)) {
+					System.out.println("Porta " + porta + " está em uso!");
+				}
 			}
-			
+
 			porta++;
 		}
-		
+
 		System.out.println("--> Fim do escaneamento.");
 	}
 }
--- src/br/com/imd/protocolos/tcp/portscanner/remoto/RemotePortScanner.java
+++ src/br/com/imd/protocolos/tcp/portscanner/remoto/RemotePortScanner.java
@@ -1,22 +1,35 @@
 package br.com.imd.protocolos.tcp.portscanner.remoto;
 
 import java.io.IOException;
-import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketAddress;
+import java.util.Scanner;
 
 public class RemotePortScanner {
 
 	public static int porta = 1;
 	public static int portaFinal = 1024;
+	public static String enderecoRemoto = "";
+	public static String mensagemErroTimeOut = "Connection timed out: connect";
+	public static String mensagemErroEnderecoEmUso = "Address already in use: JVM_Bind";
+	public static String mensagemErroConexaoRecusada = "Connection refused: connect";
+	
+	static Scanner leitorTeclado = new Scanner(System.in);
 
 	public static void main(String[] args) throws IOException {
-		System.out.println("--> Iniciando escaneamento de portas local.");
+		System.out.println("Digite o endereço remoto a ser escaneado: ");
+		enderecoRemoto = leitorTeclado.nextLine();	
+		System.out.println("--> Iniciando escaneamento remoto de portas.");
 		while (porta <= portaFinal) {
 			try {
-				ServerSocket connectionSocket = new ServerSocket();
-			} catch (Exception e) {
-				System.out.println("Porta " + porta + " está em uso!");
+				Socket socket = new Socket(enderecoRemoto, porta);
+			} catch (Exception e) {			
+				if(e.getMessage().equals(mensagemErroConexaoRecusada)) {
+					System.out.println("Conexão recusada para a porta: " + porta);					
+				} else if (e.getMessage().equals(mensagemErroEnderecoEmUso)) {
+					System.out.println("Porta " + porta + " está em uso!");					
+				} else if (e.getMessage().equals(mensagemErroTimeOut)) {
+					System.out.println("Timeout para a porta: " + porta);					
+				}
 			}
 			
 			porta++;
